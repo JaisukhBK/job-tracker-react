f8b135b25f449e998866e24b2078a3128f3edc08
JaisukhBK <bangalorekrishnego.j@northeastern.edu>
2026-02-20 20:01:16 -0500

    Fix Supabase env variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://qaetwdpbqmowlnnyyvuc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhZXR3ZHBicW1vd2xubnl5dnVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MDMwNjEsImV4cCI6MjA4NzE3OTA2MX0.zfyRR64isumiNN5KuHPGZMEp9d1NF5WP58eBti9NYvw';  
-// paste your full key here
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhZXR3ZHBicW1vd2xubnl5dnVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MDMwNjEsImV4cCI6MjA4NzE3OTA2MX0.zfyRR64isumiNN5KuHPGZMEp9d1NF5WP58eBti9NYvw';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
